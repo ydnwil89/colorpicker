250cc06a67e8a4fc0fd727bdf0b5b6e9a5f2526b
Gleb Shamey <wwow230@gmail.com>
2022-10-04 16:25:18 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import './index.css'
 import { Breadcrumb, Layout, Menu } from 'antd'
 import { SketchPicker } from 'react-color'
 
-
 const { Header, Content, Footer } = Layout;
 
-let arr = [null, null]
-
 export const App = () => {
-
   const [color, setColor] = useState('#ffffff');
 
   return (
@@ -18,41 +14,12 @@ export const App = () => {
     <Header>
       <div className="logo">
       </div>
-      <Menu
-        theme="dark"  
-        mode="horizontal"
-        defaultSelectedKeys={['2']}
-        items={
-          arr.map((_, index) => {
-          const key = index + 1;
-          if (key === 1) {
-            return {
-              key,
-              label: 'Home',
-            };
-          }
-          else {
-            return {
-              key,
-              label: 'Colorpicker',
-            }
-          }
-        })}
-      />
     </Header>
     <Content
       style={{
-        padding: '0 50px',
+        padding: '10 50px',
       }}
     >
-      <Breadcrumb
-        style={{
-          margin: '16px 0',
-        }}
-      >
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>Colorpicker</Breadcrumb.Item>
-      </Breadcrumb>
       <div className="site-layout-content" style={{ backgroundColor: color }}>
           <div className='myColorPicker'>
               <SketchPicker color={color} onChangeComplete={color => { setColor(color.hex) }} />
